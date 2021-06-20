4a17744f447c36ee17675773637a6d404072fcac
Filipe <filipedutra@outlook.com>
2021-06-20 14:30:57 -0300

    Passo 1

---
+++ EcoSistemas/Controllers/ApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace EcoSistemas.Controllers
+{
+    public class Token
+    {
+        public string mensagem { get; set; }
+        public string proximaEtapa { get; set; }
+        public int statusCode { get; set; }
+        public string data { get; set; }
+    }
+
+    public class ApiController : Controller
+    {
+
+        //URL Base da API  
+        public string url = Config.appsettings.GetValue<string>("UrlApi");
+
+        //Buscando o Token para Autenticação 
+        public string GetToken()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer");
+
+                var response = client.GetAsync(url + "api/Desafio/GetToken").Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+
+        }
+
+    }
+}
---
+++ EcoSistemas/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EcoSistemas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EcoSistemas.Controllers
+{
+    public class HomeController : ApiController
+    {
+
+
+        public IActionResult Index()
+        {
+            //Passo 1 recebendo o valor do Token
+            var tokenDados = JsonConvert.DeserializeObject<Token>(GetToken());
+
+            ViewBag.msg = tokenDados.mensagem;
+            ViewBag.proxima = tokenDados.proximaEtapa;
+
+            return View();
+        }
+
+
+
+    }
+}
